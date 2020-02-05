4dfb2b038a8924c8994640b5e2df2204e976e13e
Thiago <titisbackup@gmail.com>
2020-02-05 00:20:35 -0300

    Finalizando implementação de salvar fotos no S3 da Amazon

--- brewer/src/main/java/com/algaworks/brewer/storage/s3/FotoStorageS3.java
+++ brewer/src/main/java/com/algaworks/brewer/storage/s3/FotoStorageS3.java
@@ -1,42 +1,106 @@
 package com.algaworks.brewer.storage.s3;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.algaworks.brewer.storage.FotoStorage;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.GroupGrantee;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.Permission;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.util.IOUtils;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 @Profile("prod")
 @Component
 public class FotoStorageS3 implements FotoStorage {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FotoStorageS3.class);
+	
+	private static final String BUCKET = "titisbrewer";
+	
+	@Autowired
+	private AmazonS3 amazonS3;
 
 	@Override
 	public String salvar(MultipartFile[] files) {
-		
-		return null;
+		String novoNome = null;
+		if(files != null && files.length > 0 ) {
+			MultipartFile arquivo = files[0];
+			novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			
+			try {
+				AccessControlList acl = new AccessControlList();
+				acl.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+				
+				enviarFoto(novoNome, arquivo, acl);				
+				enviarThumbnail(novoNome, arquivo, acl);
+			} catch (IOException e) {
+				throw new RuntimeException("Erro salvando arquivo no S3");
+			}
+			
+		}
+		return novoNome;
 	}
 
 	@Override
 	public byte[] recuperarFoto(String foto) {
-		
+		InputStream is = amazonS3.getObject(BUCKET, foto).getObjectContent();
+		try {
+			return IOUtils.toByteArray(is);
+		} catch (IOException e) {
+			LOGGER.error("Não conseguiu recuperar foto do S3", e);
+		}
 		return null;
 	}
 
 	@Override
-	public byte[] recuperarThumbnail(String fotoCerveja) {
-		
-		return null;
+	public byte[] recuperarThumbnail(String foto) {
+		return recuperarFoto(FotoStorage.THUMBNAIL_PREFIX + foto);
 	}
 
 	@Override
 	public void excluir(String foto) {
-		
-		
+		amazonS3.deleteObjects(new DeleteObjectsRequest(BUCKET).withKeys(foto, THUMBNAIL_PREFIX + foto));
 	}
 
 	@Override
 	public String getUrl(String foto) {
-		
+		if(!StringUtils.isEmpty(foto)) {
+			return "https://titisbrewer.s3-sa-east-1.amazonaws.com/" + foto;
+		}
 		return null;
+	}
+	
+	private void enviarFoto(String novoNome, MultipartFile arquivo, AccessControlList acl) throws IOException {
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentType(arquivo.getContentType());
+		metadata.setContentLength(arquivo.getSize());
+		amazonS3.putObject(new PutObjectRequest(BUCKET, novoNome, arquivo.getInputStream(), metadata).withAccessControlList(acl));
+	}
+	
+	private void enviarThumbnail(String novoNome, MultipartFile arquivo, AccessControlList acl) throws IOException {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		Thumbnails.of(arquivo.getInputStream()).height(68).toOutputStream(os);
+		byte[] array = os.toByteArray();
+		InputStream is = new ByteArrayInputStream(array);
+		ObjectMetadata thumbMetadata = new ObjectMetadata();
+		thumbMetadata.setContentType(arquivo.getContentType());
+		thumbMetadata.setContentLength(array.length);
+		amazonS3.putObject(new PutObjectRequest(BUCKET, THUMBNAIL_PREFIX + novoNome, is, thumbMetadata).withAccessControlList(acl));
 	}
 }
